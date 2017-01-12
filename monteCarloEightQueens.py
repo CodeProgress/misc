621a922ea6b62451deab43a251208dd85968cfc6
CodeProgress <codeprogress1@gmail.com>
2017-01-12 00:56:11 -0500

    Update and add all solutions

--- monteCarloEightQueens.py
+++ monteCarloEightQueens.py
@@ -1,9 +1,6 @@
 import itertools
 import random
 
-# quick first draft
-
-perms = itertools.permutations(range(4))
 
 def is_conflict(sq1, sq2):
     if sq1[0] == sq2[0]:
@@ -14,34 +11,36 @@ def is_conflict(sq1, sq2):
         return True
     return False
 
+def any_conflicts(board):
+    if len(board) <= 1:
+        return False
+    queen = board[0]
+    for q in board[1:]:
+        if is_conflict(queen, q):
+            return True
+    return any_conflicts(board[1:])
 
-board = []
-rows = range(8)
-cols = range(8)
-counter = 0
-max_num_queens = 0
 
-while len(board) < 8:
-    counter += 1
-    conflict = False
-    rand_row = random.choice(rows)
-    rand_col = random.choice(cols)
-    pos_q = (rand_row, rand_col)
-    for q in board:
-        if is_conflict(q, pos_q):
-            conflict = True
-            break
-    if not conflict:
-        rows.remove(rand_row)
-        cols.remove(rand_col)
-        board.append(pos_q)
-    else:
-        rows = range(8)
-        cols = range(8)
-        board = []
-    if len(board)> max_num_queens:
-        board_size = len(board)
-    if counter > 10000:
-        break
-        
-print board
+def get_all_solutions(size=8):
+    all_solutions = []
+    perms = itertools.permutations(range(size))
+    for board in perms:
+        sq_board = [(x, board[x]) for x in range(len(board))]
+        if not any_conflicts(sq_board):
+            all_solutions.append(board)
+    return all_solutions
+
+def solution_in_rand_permutations(size=8):
+    start = range(size)
+    num_trials = 0
+    while True:
+        num_trials += 1
+        board = random.sample(start, size)
+        sq_board = [(x, board[x]) for x in range(size)]
+        if not any_conflicts(sq_board):
+            print "Number of trials needed: {}".format(num_trials)
+            return board
+    
+
+print len(get_all_solutions())
+print solution_in_rand_permutations(8)
